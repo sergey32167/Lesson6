12c2c34a34fb39fea9b526997a500e452cf74e15
sergey32167 <serg-li-@mail.ru>
2021-07-01 16:23:56 +0300

    Add PositiveTest 1-4

--- src/test/java/test/SmokeTest.java
+++ src/test/java/test/SmokeTest.java
@@ -12,7 +12,7 @@ import pages.ProductsPage;
 public class SmokeTest {
 
     @Test
-    public void positiveLoginTest() {
+    public void positiveLoginTest1() {
         WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         WebDriver driver = new ChromeDriver();
 
@@ -24,6 +24,55 @@ public class SmokeTest {
         ProductsPage productsPage = new ProductsPage(driver);
 
         Assert.assertEquals(productsPage.getTitleText(), "PRODUCTS", "Страница Products не открылась.");
+
+        driver.quit();
+    }
+
+    @Test
+    public void positiveLoginTest2() {
+        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+        WebDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername("problem_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+        ProductsPage productsPage = new ProductsPage(driver);
+
+        Assert.assertEquals(productsPage.getTitleText(), "PRODUCTS", "Страница Products не открылась.");
+
+        driver.quit();
+    }
+
+    @Test
+    public void positiveLoginTest3() {
+        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+        WebDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername("performance_glitch_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+        ProductsPage productsPage = new ProductsPage(driver);
+
+        Assert.assertEquals(productsPage.getTitleText(), "PRODUCTS", "Страница Products не открылась.");
+
+        driver.quit();
+    }
+
+    @Test
+    public void positiveLoginTest4() {
+        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+        WebDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername("locked_out_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+        Assert.assertEquals(loginPage.getErrorLabel().getText(), "Epic sadface: Sorry, this user has been locked out." );
 
         driver.quit();
     }
