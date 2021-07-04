383e72d574f3751683378243b65692389c22594f
sergey32167 <serg-li-@mail.ru>
2021-07-04 18:01:00 +0300

    Add

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -31,4 +31,9 @@ public class ProductsPage extends BasePage {
     // Getter
     public WebElement getTitleLabel() { return driver.findElement(title_Label_By); }
     public String getTitleText() { return getTitleLabel().getText(); }
+
+    public void addToCart(String productName){
+
+    }
+
 }
---
+++ src/test/java/steps/OrderStep.java
@@ -0,0 +1,18 @@
+package steps;
+
+import baseEntities.BaseStep;
+import org.openqa.selenium.WebDriver;
+import pages.ProductsPage;
+
+public class OrderStep extends BaseStep {
+
+    public OrderStep(WebDriver driver) {
+        super(driver);
+    }
+
+    public void orderOneProduct(String productName) {
+        ProductsPage productsPage = new ProductsPage(driver, true);
+
+    }
+
+}
